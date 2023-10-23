5f971bb20c154ed606d9809ac4f695fb549f9c0f
Alex Norris <akwnorris@gmail.com>
2023-10-22 23:00:59 -0500

    Update menu pricing

--- src/recipes.js
+++ src/recipes.js
@@ -2,7 +2,7 @@ const recipes = [
     { 
       id: 1,
       title: "Greek Salad",
-      price: 12.99,
+      price: 8,
       image: "https://i.ibb.co/68c9bLv/greek-salad.jpg",
       description:
         "Crisp cucumbers, tomatoes, olives, and feta, drizzled with olive oil and oregano.",
@@ -10,7 +10,7 @@ const recipes = [
     {
       id: 2,
       title: "Bruchetta",
-      price: 5.99,
+      price: 10,
       image: "https://i.ibb.co/317GqZv/Bruchetta.png",
       description:
         "Freshly toasted Italian bread topped with ripe tomatoes, basil, and balsamic glaze.",
@@ -18,7 +18,7 @@ const recipes = [
     {
       id: 3,
       title: "Nonna's Lemon Cake",
-      price: 4.78,
+      price: 7,
       image: "https://i.ibb.co/sm6MfcH/desert.jpg",
       description:
         "A zesty delight with layers of moist lemon-infused cake and a tangy lemon glaze.",
