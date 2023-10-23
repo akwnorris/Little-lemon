2cd22503f6730fe12936bc31ef955d09619070bb
Alex Norris <akwnorris@gmail.com>
2023-10-23 10:44:59 -0500

    Change time format to 12 hour

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -20,6 +20,14 @@ const BookingForm = (props) => {
     props.dispatch(e);
    }
 
+   const formatTime = (time) => {
+    // Format time from 24-hour to 12-hour format
+    const [hours, minutes] = time.split(':');
+    const isPM = parseInt(hours) >= 12;
+    const formattedHours = isPM ? parseInt(hours) - 12 : parseInt(hours);
+    return `${formattedHours}:${minutes} ${isPM ? 'PM' : 'AM'}`;
+  };
+
   return (
     <header>
       <section>
@@ -27,29 +35,58 @@ const BookingForm = (props) => {
           <fieldset className="formField">
             <div>
               <label htmlFor="book-date">Choose Date:</label>
-              <input id="book-date" value={date} onChange={(e) => handleChange(e.target.value)} type="date" required/>
+              <input
+                id="book-date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+                type="date"
+                required
+              />
             </div>
             <div>
               <label htmlFor="book-time">Choose Time:</label>
-              <select id="book-time" value={times} onChange={(e) => setTimes(e.target.value)} required>
+              <select
+                id="book-time"
+                value={times}
+                onChange={(e) => handleChange(e)}
+                required
+              >
                 <option value="">Select a Time</option>
-               {props.availableTimes.availableTimes.map(availableTimes => {return <option key={availableTimes}>{availableTimes}</option>})}
+                {props.availableTimes.availableTimes.map((availableTime) => (
+                  <option key={availableTime} value={availableTime}>
+                    {formatTime(availableTime)}
+                  </option>
+                ))}
               </select>
             </div>
             <div>
               <label htmlFor="book-guests">Number of Guests:</label>
-              <input id="book-guests" min="1" value={guests} onChange={(e) => {setGuests(e.target.value)}} type={"number"} placeholder={0} max={10} required></input>
+              <input
+                id="book-guests"
+                min="1"
+                value={guests}
+                onChange={(e) => setGuests(e.target.value)}
+                type="number"
+                placeholder="0"
+                max="10"
+                required
+              />
             </div>
             <div>
               <label htmlFor="book-occasion">Occasion:</label>
-              <select id="book-occasion" key={occasion} value={occasion} onChange={(e) => setOccasion(e.target.value)} required>
+              <select
+                id="book-occasion"
+                value={occasion}
+                onChange={(e) => setOccasion(e.target.value)}
+                required
+              >
                 <option value="">Select an Option</option>
                 <option>Birthday</option>
                 <option>Anniversary</option>
               </select>
             </div>
             <div className="btnReceive">
-              <input aria-label="On Click" type={"submit"} value={"Make Your Reservation"}></input>
+              <input aria-label="On Click" type="submit" value="Make Your Reservation" />
             </div>
           </fieldset>
         </form>
